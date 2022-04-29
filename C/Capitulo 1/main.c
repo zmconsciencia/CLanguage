2bb854201c09a081a05753eafde12e33b2d7e35f
zmconsciencia <94399930+zmconsciencia@users.noreply.github.com>
2022-04-29 10:00:22 +0100

    Chapter 1 completed

---
+++ C/Capitulo 1/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    /*
+    printf("C\nis the greatest\nlanguage.\n");
+    return 0;
+    */
+    /*
+    printf("Hoje está um \"LINDO\" dia.\n");
+    */
+    /*(esta linha de codigo pode ser escrita com aspas simples '' em vez de \, mas esta escrito do modo para focar no ponto do carater especial \ */
+
+    //Exercicios Resolvidos
+    //1.
+    /*
+    printf("1 -\tClientes\n"); //\t para colocar uma tabulação
+    printf("2 -\tFornecedores\n");
+    printf("3 -\tFaturas\n\n"); //duplo \n para adicionar uma linha em branco
+    printf("4 -\tSair\n");
+    */
+
+    //2.
+    /*
+    printf("Aqui vai um apito\n\a"); // \a emite sinal sonoro
+    printf("Aqui vai um apito\n\7"); // \7 emite um sinal sonoro
+    */
+
+    //Exercicios Propostos (os que não são programas estão escritos no livro)
+    //15
+    /*
+    printf("Bem vindos ao \/Mundo\\ da programação em \"C\"\n");
+    */
+
+    //16
+    /*
+    printf("  *  \n");
+    printf(" *** \n");
+    printf("*****\n");
+    printf(" /|\\\n");
+    */
+
+    //17
+    /*
+    printf("Total\t=\t100%%\n");
+    printf("IVA\t=\t17%%\n");
+    printf("IRS\t=\t15%%\n");
+    printf("-----------------------\n");
+    printf("Liq.\t=\t68%%\n");
+    */
+    //18
+
+    puts("Hello World");
+
+}
